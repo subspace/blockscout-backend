gateway-fm/blockscout

Merge addresses stage with address referencing

apps/explorer/lib/explorer/chain/import.ex
@@ -12,8 +12,7 @@ defmodule Explorer.Chain.Import do
   require Logger
 
   @stages [
-    Import.Stage.Addresses,
-    Import.Stage.AddressReferencing,
+    Import.Stage.AddressesBlocks,
     Import.Stage.BlockReferencing,
     Import.Stage.BlockFollowing,
     Import.Stage.BlockPending

apps/explorer/lib/explorer/chain/import/stage/block_following.ex
@@ -1,9 +1,7 @@
 defmodule Explorer.Chain.Import.Stage.BlockFollowing do
   @moduledoc """
   Imports any tables that follows and cannot be imported at the same time as
-  those imported by `Explorer.Chain.Import.Stage.Addresses`,
-  `Explorer.Chain.Import.Stage.AddressReferencing` and
-  `Explorer.Chain.Import.Stage.BlockReferencing`
+  those imported by `Explorer.Chain.Import.Stage.AddressesBlocks` and `Explorer.Chain.Import.Stage.BlockReferencing`
   """
 
   alias Explorer.Chain.Import.{Runner, Stage}

apps/explorer/lib/explorer/chain/import/stage/block_pending.ex
@@ -2,9 +2,7 @@ defmodule Explorer.Chain.Import.Stage.BlockPending do
   @moduledoc """
   Imports any tables that uses `Explorer.Chain.PendingBlockOperation` to track
   progress and cannot be imported at the same time as those imported by
-  `Explorer.Chain.Import.Stage.Addresses`,
-  `Explorer.Chain.Import.Stage.AddressReferencing` and
-  `Explorer.Chain.Import.Stage.BlockReferencing`
+  `Explorer.Chain.Import.Stage.AddressesBlocks` and `Explorer.Chain.Import.Stage.BlockReferencing`
   """
 
   alias Explorer.Chain.Import.{Runner, Stage}

apps/explorer/lib/explorer/chain/import/stage/block_referencing.ex
@@ -1,8 +1,7 @@
 defmodule Explorer.Chain.Import.Stage.BlockReferencing do
   @moduledoc """
   Imports any tables that reference `t:Explorer.Chain.Block.t/0` and that were
-  imported by `Explorer.Chain.Import.Stage.Addresses` and
-  `Explorer.Chain.Import.Stage.AddressReferencing`.
+  imported by `Explorer.Chain.Import.Stage.AddressesBlocks`.
   """
 
   alias Explorer.Chain.Import.{Runner, Stage}

subspace/blockscout-backend

apps/explorer/lib/explorer/chain/import.ex
@@ -12,8 +12,7 @@ defmodule Explorer.Chain.Import do
   require Logger
 
   @stages [
-    Import.Stage.Addresses,
-    Import.Stage.AddressReferencing,
+    Import.Stage.AddressesBlocksCoinBalances,
     Import.Stage.BlockReferencing,
     Import.Stage.BlockFollowing,
     Import.Stage.BlockPending

apps/explorer/lib/explorer/chain/import/stage/block_following.ex
@@ -1,9 +1,7 @@
 defmodule Explorer.Chain.Import.Stage.BlockFollowing do
   @moduledoc """
   Imports any tables that follows and cannot be imported at the same time as
-  those imported by `Explorer.Chain.Import.Stage.Addresses`,
-  `Explorer.Chain.Import.Stage.AddressReferencing` and
-  `Explorer.Chain.Import.Stage.BlockReferencing`
+  those imported by `Explorer.Chain.Import.Stage.AddressesBlocksCoinBalances` and `Explorer.Chain.Import.Stage.BlockReferencing`
   """
 
   alias Explorer.Chain.Import.{Runner, Stage}

apps/explorer/lib/explorer/chain/import/stage/block_pending.ex
@@ -2,9 +2,7 @@ defmodule Explorer.Chain.Import.Stage.BlockPending do
   @moduledoc """
   Imports any tables that uses `Explorer.Chain.PendingBlockOperation` to track
   progress and cannot be imported at the same time as those imported by
-  `Explorer.Chain.Import.Stage.Addresses`,
-  `Explorer.Chain.Import.Stage.AddressReferencing` and
-  `Explorer.Chain.Import.Stage.BlockReferencing`
+  `Explorer.Chain.Import.Stage.AddressesBlocksCoinBalances` and `Explorer.Chain.Import.Stage.BlockReferencing`
   """
 
   alias Explorer.Chain.Import.{Runner, Stage}

apps/explorer/lib/explorer/chain/import/stage/block_referencing.ex
@@ -1,8 +1,7 @@
 defmodule Explorer.Chain.Import.Stage.BlockReferencing do
   @moduledoc """
   Imports any tables that reference `t:Explorer.Chain.Block.t/0` and that were
-  imported by `Explorer.Chain.Import.Stage.Addresses` and
-  `Explorer.Chain.Import.Stage.AddressReferencing`.
+  imported by `Explorer.Chain.Import.Stage.AddressesBlocksCoinBalances`.
   """
 
   alias Explorer.Chain.Import.{Runner, Stage}